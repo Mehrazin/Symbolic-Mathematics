5ba30bc59f02d6eb166c5da9d12fe095c53adc6f
Mehrazin <mehrazinalizadeh@gmail.com>
2021-04-11 18:46:45 -0400

    Added a config module that keeps track of the configurations
    config.py: this module contains Config class. Config class has the following groups of attributes: 1) Paths related to the experiment, 2) Experiment configurations like experiment id. 3) Environment specs.
    Exvironment specs are mostly related to the setup of SymPy.
    
    Upon instantiation, Config class checks if there exists a Dumped directory or not. If not, it will be created. Then, Config class generates an experiment id with get_exp_id method. A directory with the name of experiment id will be created in the  Dumped directory.

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+class Config():
+    def __init__(self):
+        # Define dataset path
+        self.exp_type = 'Prime_BWD'
+        self.Dataset_dir = os.path.join(os.getcwd(), 'Dataset' + self.exp_type)
+        self.train_dir = os.path.join(self.Dataset_dir, 'prim_bwd.train')
+        self.test_dir = os.path.join(self.Dataset_dir, 'prim_bwd.test')
+        self.valid_dir = os.path.join(self.Dataset_dir, 'prim_bwd.valid')
+
+        # Experiment path
+        self.dump_path = os.path.join(os.getcwd(), 'Dumped')
+        if not os.path.exists(self.dump_path):
+            os.mkdir(self.dump_path)
+        self.exp_id = self.get_exp_id()
+        self.exp_dir = os.path.join(self.dump_path, str(self.exp_id))
+        if not os.path.exists(self.exp_dir):
+            os.mkdir(self.exp_dir)
+
+        self.task = 'prim_bwd'
+        self.rewrite_functions = ''
+        self.clean_prefix_expr = True
+
+        self.eos_index = 0
+        self.pad_index = 1
+
+    def get_exp_id(self):
+        """
+        Returns an integer as an experiment id.
+        """
+        dir_list = os.listdir(self.dump_path)
+        if len(dir_list) == 0 :
+            id = 1
+        else :
+            dir_list = [int(dir) for dir in dir_list]
+            id = max(dir_list) + 1
+        return id
