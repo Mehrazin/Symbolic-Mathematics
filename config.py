ce859629f48cd55c843af2ddd709bff1418ab694
Mehrazin <mehrazinalizadeh@gmail.com>
2021-04-15 18:27:36 -0400

    Added some experiment-related and training-related features to the Config class
    Experiment-related features: reload_size, which determines how many samples we are going to load from the training dataset. env_seed, which set the seed for the current experiment.
    
    Traininf_related features: epoch_size: determines the size of each epooch during the training. Note: in the original implementation, authors did not set the epooch size to be the training data size. Instead, fro each epooch they get their samples from the dataset with a rendom index. batch_size: a feature that defines the batch size. learning_rate: a feature that stores the value of learning_rate

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config():
     def __init__(self):
         # Define dataset path
         self.exp_type = 'Prime_BWD'
-        self.Dataset_dir = os.path.join(os.getcwd(), 'Dataset' + self.exp_type)
+        self.Dataset_dir = os.path.join(os.getcwd(), 'Dataset' , self.exp_type)
         self.train_dir = os.path.join(self.Dataset_dir, 'prim_bwd.train')
         self.test_dir = os.path.join(self.Dataset_dir, 'prim_bwd.test')
         self.valid_dir = os.path.join(self.Dataset_dir, 'prim_bwd.valid')
@@ -18,12 +18,20 @@ class Config():
         if not os.path.exists(self.exp_dir):
             os.mkdir(self.exp_dir)
 
+        # Experimental setup
         self.task = 'prim_bwd'
         self.rewrite_functions = ''
         self.clean_prefix_expr = True
-
+        self.env_seed = 0
+        self.reload_size = 40000
         self.eos_index = 0
         self.pad_index = 1
+
+        #Training configurations
+        self.epoch_size = 300000
+        self.batch_size = 10
+        self.learning_rate = 0.0001
+
 
     def get_exp_id(self):
         """
